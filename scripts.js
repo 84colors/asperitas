aaa4027b3f3be63f9973ceca519a0f95817c52fa
Cristiana Bardeanu <kit10designs@gmail.com>
2024-01-08 13:53:16 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const isLocal = true;
 
 console.log("jeelo from desktop??");
@@ -19,13 +21,13 @@ const tlSearch = gsap.timeline({
 
 tlSearch.to(searchInputWrap, {
     width: "100%",
-    duration: 0.5,
+    duration: 0.6,
 });
 tlSearch.to(
     searchIcon,
     {
         opacity: 0,
-        duration: 0.4,
+        duration: 0.3,
     },
     "<"
 );
@@ -41,13 +43,14 @@ tlSearch.to(
     searchIClose,
     {
         opacity: 1,
-        duration: 0.4,
+        duration: 0.3,
     },
-    "<0.25"
+    "<0.1"
 );
 
 //On click of search icon, resize input and show close btn, fade nav, add focus
 $(searchIcon).on("click", function () {
+    tlSearch.timeScale(1);
     tlSearch.restart();
     searchInput.focus();
     $(this).css("pointer-events", "none");
@@ -55,6 +58,7 @@ $(searchIcon).on("click", function () {
 
 //On click of close btn, reverse timeline, clear value
 $(searchIClose).on("click", function () {
+    tlSearch.timeScale(1.5);
     tlSearch.reverse();
     searchInput.val("");
     searchIcon.css("pointer-events", "auto");
