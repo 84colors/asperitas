ecda6c89b25cf61085109d39170b93bd3c701fe8
Cristiana Bardeanu <kit10designs@gmail.com>
2024-01-08 13:45:25 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -6,13 +6,58 @@ console.log("jeelo from desktop??");
 // Toggle Search
 // //
 const searchInput = $(".search-wrap .v");
+const searchInputWrap = $(".search-field-wrap");
 const searchIcon = $(".search-wrap .icon-search");
 const searchIClose = $(".search-wrap .icon-search-close");
+const siteNav = $(".nav-links");
 
 //Set input size init
+const tlSearch = gsap.timeline({
+    paused: true,
+    ease: "power4.in0ut",
+});
 
-//On click of search icon, resize input and show close btn, fade nav
+tlSearch.to(searchInputWrap, {
+    width: "100%",
+    duration: 0.5,
+});
+tlSearch.to(
+    searchIcon,
+    {
+        opacity: 0,
+        duration: 0.4,
+    },
+    "<"
+);
+tlSearch.to(
+    siteNav,
+    {
+        opacity: 0.5,
+        duration: 0.5,
+    },
+    0
+);
+tlSearch.to(
+    searchIClose,
+    {
+        opacity: 1,
+        duration: 0.4,
+    },
+    "<0.25"
+);
 
-//On click of close btn, reverse timeline
+//On click of search icon, resize input and show close btn, fade nav, add focus
+$(searchIcon).on("click", function () {
+    tlSearch.restart();
+    searchInput.focus();
+    $(this).css("pointer-events", "none");
+});
+
+//On click of close btn, reverse timeline, clear value
+$(searchIClose).on("click", function () {
+    tlSearch.reverse();
+    searchInput.val("");
+    searchIcon.css("pointer-events", "auto");
+});
 
 ////
