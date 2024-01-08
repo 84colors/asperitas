507559a65dd4cf080cf15215b04cf41bdf7ab720
Cristiana Bardeanu <kit10designs@gmail.com>
2024-01-08 12:37:50 +0100

    update

--- scripts.js
+++ scripts.js
@@ -1,1 +1,18 @@
-console.log("jeelo from desktop");
+const isLocal = true;
+
+console.log("jeelo from desktop??");
+
+// //
+// Toggle Search
+// //
+const searchInput = $(".search-wrap .v");
+const searchIcon = $(".search-wrap .icon-search");
+const searchIClose = $(".search-wrap .icon-search-close");
+
+//Set input size init
+
+//On click of search icon, resize input and show close btn, fade nav
+
+//On click of close btn, reverse timeline
+
+////
