6bde57fc4eea4958c0847cc501d58dcc232ad54b
Cristiana Bardeanu <kit10designs@gmail.com>
2024-02-27 11:52:22 +0100

    Update mv-scripts.js

--- mv-scripts.js
+++ mv-scripts.js
@@ -1,59 +1,66 @@
 // gsap.registerPlugin(ScrollTrigger);
 "use strict";
-const isLocalMV = true;
+// const isLocalMV = true;
 
-//Get model-viewer with ID of model
-const mv = document.querySelector("#model");
-const mvJ = $("#model");
+$(document).ready(function () {
+    //Get model-viewer with ID of model
+    const mv = document.querySelector("#model");
+    const mvJ = $("#model");
 
-const btnOpen = $(".btn-open");
-const btnClose = $(".btn-close");
+    const btnOpen = $(".btn-open");
+    const btnClose = $(".btn-close");
 
-// --------------------
-// Toggle anim on click
-// --------------------
-mv.addEventListener("load", (e) => {
-    let isOpen = true;
+    // console.log("hello???");
+
+    btnOpen.show();
     btnClose.hide();
 
-    // if (isOpen) {
-    //     btnOpen.on("click", function () {
-    //         mv.animationName = "MoveUp";
-    //         mv.timeScale = 1;
-    //         mv.play({ repetitions: 1 });
-    //     });
-    //     isOpen = false;
-    // } else {
-    //     btnOpen.on("click", function () {
-    //         mv.animationName = "MoveUp";
-    //         mv.timeScale = -1;
-    //         mv.play({ repetitions: 1 });
-    //     });
-    //     isOpen = true;
-    // }
+    // --------------------
+    // Toggle anim on click
+    // --------------------
+    mv.addEventListener("load", (e) => {
+        let isOpen = true;
+        btnClose.hide();
 
-    btnOpen.on("click", function () {
-        mv.animationName = "ArmatureAction.001";
-        mv.timeScale = 1;
-        mv.play({ repetitions: 1 });
-        $(this).hide();
-        btnClose.show();
+        // if (isOpen) {
+        //     btnOpen.on("click", function () {
+        //         mv.animationName = "MoveUp";
+        //         mv.timeScale = 1;
+        //         mv.play({ repetitions: 1 });
+        //     });
+        //     isOpen = false;
+        // } else {
+        //     btnOpen.on("click", function () {
+        //         mv.animationName = "MoveUp";
+        //         mv.timeScale = -1;
+        //         mv.play({ repetitions: 1 });
+        //     });
+        //     isOpen = true;
+        // }
+
+        btnOpen.on("click", function () {
+            mv.animationName = "ArmatureAction.001";
+            mv.timeScale = 1;
+            mv.play({ repetitions: 1 });
+            $(this).hide();
+            btnClose.show();
+        });
+        btnClose.on("click", function () {
+            mv.timeScale = -1;
+            mv.animationName = "ArmatureAction.001";
+            mv.play({ repetitions: 1 });
+            $(this).hide();
+            btnOpen.show();
+        });
     });
-    btnClose.on("click", function () {
-        mv.timeScale = -1;
-        mv.animationName = "ArmatureAction.001";
-        mv.play({ repetitions: 1 });
-        $(this).hide();
-        btnOpen.show();
+
+    // --------------------
+    // Play GLTF animation. note: bug with repetion when using name
+    // --------------------
+
+    mv.addEventListener("camera-change", (e) => {
+        let cameraTargetNew = mv.getCameraTarget();
+        let cameraOrbitNew = mv.getCameraOrbit();
+        // console.log(`Target: ${cameraTargetNew};`, `Orbit: ${cameraOrbitNew}`);
     });
-});
-
-// --------------------
-// Play GLTF animation. note: bug with repetion when using name
-// --------------------
-
-mv.addEventListener("camera-change", (e) => {
-    let cameraTargetNew = mv.getCameraTarget();
-    let cameraOrbitNew = mv.getCameraOrbit();
-    // console.log(`Target: ${cameraTargetNew};`, `Orbit: ${cameraOrbitNew}`);
 });
