0c992cfd047ee16960e5dea8b06acba9a35f2179
Cristiana Bardeanu <kit10designs@gmail.com>
2026-02-24 16:39:33 +0100

    faq

--- scripts.js
+++ scripts.js
@@ -115,3 +115,32 @@ $(".splash").ripples({
     dropRadius: 15,
     perturbance: 0.03,
 });
+
+// FAQ TOGGLES
+// -------------------
+// [faq='faq-container'] on container of all items, usually a flex, with item as direct child
+// [faq='faq-item-content'] on container of text to be hidden, gets 0 height by default and height auto on 'is-active' class
+// [faq='faq-item-img'] on icon that spins
+// -------------------
+let faqContainer = $("[faq='faq-container']");
+
+faqContainer.each(function () {
+    let faqItem = $(this).find($("[faq='faq-container'] > div"));
+
+    // add a timeline for each tab item and pass index
+    faqItem.each(function () {
+        let faqContent = $(this).find("[faq='faq-item-content']");
+        let faqContentImg = $(this).find("[faq='faq-item-img']");
+        //on click active class to clicked tab and play timeline
+        $(this).on("click", function () {
+            faqItem.removeClass("is-active");
+            faqItem
+                .find($("[faq='faq-item-content']"))
+                .removeClass("is-active");
+            faqItem.find($("[faq='faq-item-img']")).removeClass("is-active");
+            $(this).toggleClass("is-active");
+            faqContentImg.toggleClass("is-active");
+            faqContent.toggleClass("is-active");
+        });
+    });
+});
